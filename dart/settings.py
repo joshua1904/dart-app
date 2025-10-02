53457e52836326b891cb1980130a12783fecd02a
Joshua Tewes <joshua.tewes@check24.de>
2025-10-02 21:47:50 +0200

    s

--- dart/settings.py
+++ dart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY", default="test")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,*", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
 
 
 # Application definition
